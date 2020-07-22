417f19f7ba3edb5ba998606e33f03b059433f239
unknown <sachinkn2019@gmail.com>
2020-07-22 09:55:51 +0530

    first commit

--- p3/views.py
+++ p3/views.py
@@ -8,4 +8,15 @@ def home(request):
     return render(request,"first.html")
 
 def second(request):
-    return render(request,"directory/second.html")    
+    return render(request,"directory/second.html") 
+
+def third(request):
+    return render(request,"directory/third.html",context={'data':"sachin",'name':"nagesh"}) 
+
+def fourth(request):
+    fruits=['strawberry','cherry','mango']
+    return render(request,"directory/fourth.html",{'fruits':fruits}) 
+
+def fifth(request):
+    return render(request,"directory/fifth.html",{'a':10,'b':100}) 
+            
